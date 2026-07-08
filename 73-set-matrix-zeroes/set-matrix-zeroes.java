605b9d647838fbfdcd6561f58d59b29526520b02
yashwanth-reddyy <yashwanthreddybolladi441@gmail.com>
2026-07-08 12:00:10 +0530

    Time: 3 ms (20.17%) | Memory: 47.7 MB (43.42%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -5,12 +5,8 @@ class Solution {
         for(int i =0;i<matrix.length;i++){
             for(int j =0;j<matrix[i].length;j++){
                 if(matrix[i][j]==0){
-                    if(!rows.contains(i)){
                         rows.add(i);
-                    }
-                    if(!col.contains(j)){
                         col.add(j);
-                    }
                 }
             }
         }
