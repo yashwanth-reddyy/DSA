bb0ba403c0a39e5e24205da99201ecb008fa0df5
yashwanth-reddyy <yashwanthreddybolladi441@gmail.com>
2026-07-08 11:30:30 +0530

    Time: 29 ms (81.15%) | Memory: 95.8 MB (28.08%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -3,29 +3,22 @@ class Solution {
         if(nums.length==0||nums.length==1){
             return nums.length;
         }
-        HashMap<Integer,Integer> y =new HashMap<>();
+        HashSet<Integer> x =new HashSet<>();
         for(int i =0;i<nums.length;i++){
-            y.put(nums[i],1);
+            x.add(nums[i]);
         }
         int length=1;
-        int count=1;
-        for(int i=0;i<nums.length;i++){
-            int n=nums[i];
-            int u =nums[i];
-            count=1;
-            y.remove(n);
-            while(y.containsKey(n+1)){
-                count++;
-                y.remove(n+1);
-                n++;
-            }
-            while(y.containsKey(u-1)){
-                count++;
-                y.remove(u-1);
-                u--;
-            }
-            if(count>length){
-                length=count;
+        for(int i :x){
+            if(!x.contains(i-1)){
+                int max=1;
+                int num=i;
+                while(x.contains(num+1)){
+                    num++;
+                    max++;
+                }
+                if(max>length){
+                    length=max;
+                }
             }
         }
         return length;
