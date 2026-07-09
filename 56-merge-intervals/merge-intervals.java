1c3c0b067af3d38a3d859bfc3ef1cace1785c654
yashwanth-reddyy <yashwanthreddybolladi441@gmail.com>
2026-07-09 23:18:05 +0530

    Time: 10 ms (21.88%) | Memory: 48.8 MB (92.01%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+     List<int[]>y = new ArrayList<>();
+     int start =intervals[0][0];
+     int end=intervals[0][1];
+     int ans[]=new int[2];
+     ans[0]=start;
+     ans[1]=end;
+     y.add(ans);
+     for(int i =1;i<intervals.length;i++){
+       int start1 =intervals[i][0];
+       int end1=intervals[i][1];
+       if(start1>=start&&start1<=end){
+        if(y.size()>0){
+            y.remove(y.size()-1);
+        }
+            int u =start>=start1?start1:start;
+            int p=end1>=end?end1:end;
+            int ans1[]=new int[2];
+           ans1[0]=u;
+           ans1[1]=p;
+           start=u;
+           y.add(ans1);
+           end=p;
+       }
+       else{
+        int ans1[]=new int[2];
+           ans1[0]=start1;
+           ans1[1]=end1;
+           y.add(ans1);
+           start=start1;
+           end=end1;
+       }
+     }
+     int arr[][]=new int[y.size()][2];
+    for(int k =0;k<y.size();k++){
+        int arr2[]=y.get(k);
+        arr[k][0]=arr2[0];
+        arr[k][1]=arr2[1];
+    }
+    return arr;
+    }
+}
