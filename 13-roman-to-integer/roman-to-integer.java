b81212c44a8eb4919b1d25ebf2f9fbcd481d1470
yashwanth-reddyy <yashwanthreddybolladi441@gmail.com>
2026-07-22 15:36:07 +0530

    Time: 5 ms (33.42%) | Memory: 47.2 MB (6.25%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -15,10 +15,10 @@ class Solution {
             if(((s.charAt(i)=='V')||(s.charAt(i)=='X'))&&s.charAt(i-1)=='I'){
                 ans-=2;
             }
-            if(((s.charAt(i)=='L')||(s.charAt(i)=='C'))&&s.charAt(i-1)=='X'){
+            else if(((s.charAt(i)=='L')||(s.charAt(i)=='C'))&&s.charAt(i-1)=='X'){
                 ans-=20;
             }
-             if(((s.charAt(i)=='D')||(s.charAt(i)=='M'))&&s.charAt(i-1)=='C'){
+            else if(((s.charAt(i)=='D')||(s.charAt(i)=='M'))&&s.charAt(i-1)=='C'){
                 ans-=200;
             }
             }
