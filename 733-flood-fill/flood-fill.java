79444351caacf6963815feffbc18645ad45ea38d
yashwanth-reddyy <yashwanthreddybolladi441@gmail.com>
2026-08-23 17:20:45 +0530

    Time: 2 ms (11.39%) | Memory: 46.9 MB (25.18%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,25 +1,39 @@
+class Pair{
+    int i;
+    int j;
+    public Pair(int i ,int j){
+        this.i=i;
+        this.j=j;
+    }
+}
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        dfs(image,sr,sc,color,image[sr][sc]);
+        Queue<Pair> t=new LinkedList<>();
+        int original=image[sr][sc];
+        Pair p =new Pair(sr,sc);
+        t.add(p);
+        while(!t.isEmpty()){
+            Pair r=t.poll();
+            int s=r.i;
+            int g=r.j;
+            image[s][g]=color;
+            if((s-1>-1&&s-1<image.length)&&(image[s-1][g]!=color&&image[s-1][g]==original)){
+                Pair q=new Pair(s-1,g);
+                t.add(q);
+            }
+            if((s+1>-1&&s+1<image.length)&&(image[s+1][g]!=color&&image[s+1][g]==original)){
+                Pair q=new Pair(s+1,g);
+                t.add(q);
+            }
+            if((g+1>-1&&g+1<image[0].length)&&(image[s][g+1]!=color&&image[s][g+1]==original)){
+                Pair q=new Pair(s,g+1);
+                t.add(q);
+            }
+            if((g-1>-1&&g-1<image[0].length)&&image[s][g-1]!=color&&image[s][g-1]==original){
+                Pair q=new Pair(s,g-1);
+                t.add(q);
+            }
+        }
         return image;
-    }
-    public void dfs(int[][] image, int sr, int sc, int color,int original){
-        if(sr<0||sr>=image.length){
-            return;
-        }
-        if(sc<0||sc>=image[0].length){
-            return;
-        }
-        if(image[sr][sc]!=original){
-            return;
-        }
-        if(image[sr][sc]==color){
-            return;
-        }
-        image[sr][sc]=color;
-        dfs(image,sr+1,sc,color,original);
-        dfs(image,sr,sc+1,color,original);
-        dfs(image,sr,sc-1,color,original);
-         dfs(image,sr-1,sc,color,original);
     }
 }
