c9a682ef46266b30e1124b64ae6e512d1c19a260
yashwanth-reddyy <yashwanthreddybolladi441@gmail.com>
2026-09-06 21:51:50 +0530

    Time: 2 ms (38.55%) | Memory: 47.2 MB (37.67%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,30 +1,27 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-         int[] color = new int[graph.length];
-
-        Arrays.fill(color, -1);
-
-        for (int i = 0; i < graph.length; i++) {
-            if (color[i] != -1) continue;
-
-            Queue<Integer> queue = new LinkedList<>();
-            queue.offer(i);
-            color[i] = 0;
-
-            while (!queue.isEmpty()) {
-                int node = queue.poll();
-
-                for (int neighbor : graph[node]) {
-                    if (color[neighbor] == -1) {
-                        color[neighbor] = 1 - color[node];
-                        queue.offer(neighbor);
-                    } else if (color[neighbor] == color[node]) {
+          int color[]=new int[graph.length];
+          Arrays.fill(color,-1);
+          for(int i =0;i<graph.length;i++){
+            if(color[i]!=-1){
+                continue;
+            }
+            color[i]=0;
+            Queue<Integer> t = new LinkedList<>();
+            t.add(i);
+            while(!t.isEmpty()){
+                int index=t.poll();
+                for(int j =0;j<graph[index].length;j++){
+                    if(color[graph[index][j]]==-1){
+                        color[graph[index][j]]=1-color[index];
+                        t.add(graph[index][j]);
+                    }
+                    else if(color[graph[index][j]]==color[index]){
                         return false;
                     }
                 }
             }
-        }
-
-        return true;
+          }
+          return true;
     }
 }
